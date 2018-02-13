e496d73c00316791357b11b3a1f941a6bfd31357
Chris Lomonico <chlomoni@microsoft.com>
2018-02-13 15:56:32 +0100

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=hashackbot-Web-Deploy.pubxml -p:Password=BmAexRL1H2rFbWTrer5jtF4ZDeANN18meEaaFis7tGNQ7ymMzW7pcjmviQom
+
