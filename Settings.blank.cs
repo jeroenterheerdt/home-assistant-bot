a0eafc05b2ee6ebd67433b6f8b75d2297c4e2a49
Oliver Scheer <oliver.scheer@hotmail.de>
2018-02-14 10:32:10 +0100

    Added settings file

---
+++ Settings.blank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleEchoBot
+{
+    public partial class Settings
+    {
+        public Settings()
+        {
+            this.BotName = "Cortana, the Home Assistant";
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleEchoBot
+{
+    public partial class Settings
+    {
+        // Sample Settings
+        public string BotName { get; set; }
+    }
+}
